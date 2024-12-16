f60eea02311d906cd8b52967041e0419710134cf
valen-mascarenhas14 <100989126+valen-mascarenhas14@users.noreply.github.com>
2024-12-16 18:05:02 +0530

    Update test.ps1

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,4 +1,13 @@
-Write-Output "Hello, World!"
+
 
 $resourcegroupName = "test-123"
-Write-Output "rg_name=$resourcegroupName" >> $Env:GITHUB_ENV
+
+$final="/name/${resourcegroupName}"
+
+$final1="/name/$resourcegroupName"
+#Write-Output "rg_name=$resourcegroupName" >> $Env:GITHUB_ENV
+
+Write-Output $final
+
+Write-Output "Hello, World!"
+Write-Output $final1
